a0940ffec10a73e1cf3f0420d20db5db8f1157d5
gkumar-pandey <gautamshekhar078@gmail.com>
2024-02-18 23:38:47 +0530

    fix: routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const {
+  getAllStudentsHandler,
+  addNewStudentHandler,
+} = require("../controllers/student.controller");
+
 const routes = express.Router();
 
-routes.use("/api", require("./api"));
+// routes.use("/api", require("./api"));
 routes.get("/", (req, res) => {
   res.json("Hello express");
 });
+routes.get("/students", getAllStudentsHandler);
+routes.post("/students", addNewStudentHandler);
 
 module.exports = routes;
